18c04c38bb818d113ae5b4d0b7625f5906813f5f
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-10-02 17:13:27 +0200

    edit Job component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import { container, header, tags, link, main } from "./App.module.css";
+import { container, header, tags, link, main, hint } from "./App.module.css";
 import Tag from "./components/Tag/Tag";
 import Job from "./components/Job/Job";
 import data from "./data.json";
 import { v4 as uuid } from "uuid";
+import { useState, useEffect } from "react";
 
 function App() {
+  const roles = ["Frontend", "Fullstack", "Backend"];
+  const [list, setList] = useState(roles);
+  const [group, setGroup] = useState(data);
+
+  const handleRemove = (evt) => {
+    setList(list.filter((itm) => itm !== evt.target.id));
+  };
+
+  useEffect(
+    () => setGroup(group.filter((itm) => list.includes(itm.role))),
+    [list]
+  );
+
+  const handleReset = () => {
+    setList(roles);
+    setGroup(data);
+  };
+
   return (
     <div className={container}>
       <div className={header}>
         <div className={tags}>
-          <Tag />
-          <Tag />
+          {list.length > 0 ? (
+            list.map((itm) => (
+              <Tag key={uuid()} roleData={itm} removeHandler={handleRemove} />
+            ))
+          ) : (
+            <p className={hint}>Hit the reset button</p>
+          )}
 
-          <button className={link}>Clear</button>
+          <button className={link} onClick={handleReset}>
+            Reset
+          </button>
         </div>
       </div>
 
       <div className={main}>
-        {data.map((itm) => (
+        {group.map((itm) => (
           <Job key={uuid()} job={itm} />
         ))}
       </div>
--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,11 +1,17 @@
 import { container, nam, iconRemove } from "./Tag.module.css";
 import remove from "../../images/icon-remove.svg";
 
-const Tag = () => {
+const Tag = ({ roleData, removeHandler }) => {
   return (
     <div className={container}>
-      <p className={nam}>JavaScript</p>
-      <img className={iconRemove} src={remove} alt="remove icon" />
+      <p className={nam}>{roleData}</p>
+      <img
+        className={iconRemove}
+        src={remove}
+        alt="remove icon"
+        onClick={removeHandler}
+        id={roleData}
+      />
     </div>
   );
 };
